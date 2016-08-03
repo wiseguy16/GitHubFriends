9d8eb7b647bb79ac3eb90b742dcb788dd47406d0
wiseguy16 <gergusa04@gmail.com>
2016-08-03 13:29:34 -0400

    Initial with blank TableView

--- GitHubFriends/GitHubFriendsTableViewController.h
+++ GitHubFriends/GitHubFriendsTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  GitHubFriendsTableViewController.h
 //  GitHubFriends
 //
 //  Created by Gregory Weiss on 8/3/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface GitHubFriendsTableViewController : UITableViewController
 
 @end
-
