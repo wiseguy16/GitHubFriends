2f7c5ac2fa3c9c0ce447ef7c81869efab6965c3a
wiseguy16 <gergusa04@gmail.com>
2016-08-03 15:30:18 -0400

    added Modal view to enter in username

---
+++ GitHubFriends/SeachFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SeachFriendsViewController.h
+//  GitHubFriends
+//
+//  Created by Gregory Weiss on 8/3/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SeachFriendsViewController : UIViewController
+
+@end
